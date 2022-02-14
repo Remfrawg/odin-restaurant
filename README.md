ceebd76b64355a60db11a4a0cdbf7536c420ed0c
Remy Nguyen <Remytn97@gmail.com>
2022-02-14 13:07:05 -0800

    uploading restaurant page

---
+++ README.md
@@ -0,0 +1,5 @@
+#To do for the restaurant page
+
+add text to the menu and about pages
+make the menu cool? Don't be afraid to write more lines of code! 
+add link to github repo in footer
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: [ '/src/index.js', '/src/landing.js', '/src/home.js','/src/about.js','/src/menu.js'
+    ],
+    devtool: 'inline-source-map',
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname,'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: [ 'style-loader', 'css-loader'],
+            }
+        ],
+    },
+    devServer: {
+        allowedHosts: 'all',
+    }
+};
